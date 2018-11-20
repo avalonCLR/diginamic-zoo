78134ed9aee5eb2729cf166c9378dcc92bef3ea0
avalonCLR <clement.lerest@gmail.com>
2018-11-20 12:39:49 +0100

    first commit

---
+++ src/zoo/Animal.java
@@ -0,0 +1,14 @@
+package zoo;
+
+public class Animal {
+
+	private String nom;
+	private String type;
+	
+	public Animal(String nom, String type) {
+		
+		this.nom = nom;
+		this.type = type;
+	}
+	
+}
